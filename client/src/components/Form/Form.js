8a9154454cbfaf634920e4ba65f5589eb81f3375
Jatin Sonkusale <jatin22000@gmail.com>
2021-08-07 18:53:52 +0530

    minor corrections

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -43,7 +43,7 @@ const Form = ({ currentId, setCurrentId }) => {
     return (
       <Paper className={classes.paper}>
         <Typography variant="h6" align="center">
-          Please Sign In to create your own memories ans like other's memories
+          Please Sign In to create your own memories and like other's memories
         </Typography>
       </Paper>
     );
